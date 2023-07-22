0b29a216838b0d2c5e7e22d8a0bc0d90a0175396
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-07-22 20:08:12 +0200

    arweave upload issues fixed

--- app/components/text_area.tsx
+++ app/components/text_area.tsx
@@ -34,11 +34,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export function TextArea() {
-  const [assigned, setAssigned] = useState(assignees[0]);
-  const [labelled, setLabelled] = useState(labels[0]);
-  const [dated, setDated] = useState(dueDates[0]);
-
+export function TextArea({ setDescription }) {
   return (
     <div className="flex justify-center">
       <div className="w-8/12 overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
@@ -62,6 +58,7 @@ export function TextArea() {
           className="block w-full resize-none border-0 py-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 pt-4"
           placeholder="Write a description..."
           defaultValue={""}
+          onChange={(e) => setDescription(e.target.value)}
         />
 
         {/* Spacer element to match the height of the toolbar */}
--- app/product/[productId]/post-review/page.tsx
+++ app/product/[productId]/post-review/page.tsx
@@ -2,11 +2,14 @@
 
 import { TextArea } from "../../../components/text_area";
 
+import { useState } from "react";
+
 function SelectMenu({
   menuDescription,
   menuTitle,
   menuDefaultValue,
   menuOptions,
+  setter
 }) {
   return (
     <div>
@@ -21,6 +24,7 @@ function SelectMenu({
         name={menuDescription}
         className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
         defaultValue={menuDefaultValue}
+        onChange={(e) => setter(e.target.value)}
       >
         {menuOptions.map((item) => (
           <option key={item}>{item}</option>
@@ -32,12 +36,51 @@ function SelectMenu({
 
 export default function Page({ params }: { params: { productId: string } }) {
   const productId = params.productId;
+  const [description, setDescription] = useState("");
+  const [rating, setRating] = useState("5");
+  const [decentralizedStorage, setDecentralizedStorage] = useState("Arweave");
+
+  const [transaction, setTransaction] = useState("");
+  const [uploadedText, setUploadedText] = useState(false);
+
+  async function upload() {
+    if (!description || !rating) return;
+    try {
+      const reviewContentString = "[reviewContent]" + description;
+      const ratingString = "[rating]" + rating;
+      const entireReviewString = JSON.stringify(
+        reviewContentString + ratingString
+      );
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: entireReviewString
+      });
+      const json = await response.json();
+      console.log("json:", json);
+      setTransaction(json.txId);
+      setUploadedText(true);
+    } catch (err) {
+      console.log({ err });
+    }
+  }
+
+  function uploadReviewToDecentralizedStorage(e) {
+    e.preventDefault();
+    upload();
+  }
+
+  // function uploadReviewToBlockchain(e) {
+  //   e.preventDefault();
+  //   console.log(transaction);
+  //   disabled={!uploadedText}
+  //    onSubmit={uploadReviewToBlockchain}
+  // }
 
   return (
     <div className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-xl">Post Review</h1>
       <form className="w-full mt-6">
-        <TextArea />
+        <TextArea setDescription={setDescription} />
         <div className="mt-6 flex flex-col w-8/12 justify-center mx-auto">
           <div className="">
             <SelectMenu
@@ -45,24 +88,31 @@ export default function Page({ params }: { params: { productId: string } }) {
               menuTitle="Rating"
               menuDefaultValue="5"
               menuOptions={["1", "2", "3", "4", "5"]}
+              setter={setRating}
             />
           </div>
-          <div className="">
+          <div className="mt-4">
             <SelectMenu
               menuDescription="decentralizedStorage"
               menuTitle="Decentralized Storage"
               menuDefaultValue="Arweave"
-              menuOptions={["Arweave", "Filecoin"]}
+              menuOptions={["Arweave"]}
+              setter={setDecentralizedStorage}
             />
           </div>
         </div>
-        <div className="flex w-8/12 justify-end mx-auto mt-3">
-          {/* Actions: These are just examples to demonstrate the concept, replace/wire these up however makes sense for your project. */}
+        <div className="flex flex-col w-8/12 justify-end mx-auto mt-4">
+          <button
+            onClick={uploadReviewToDecentralizedStorage}
+            className="rounded-md w-full bg-slate-600 px-3 py-2 my-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            (Step 1) Upload Review to Decentralized Storage
+          </button>
           <button
             type="submit"
             className="rounded-md w-full bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Post Review
+            (Step 2) Post Review
           </button>
         </div>
       </form>
