0e05758d0e247139211738bbd3a039e78dc22d7a
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-08-01 18:23:21 -0400

    fixed font sizing

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -40,7 +40,7 @@ export default function Example() {
             <a
               key={item.name}
               href={item.href}
-              className="text-sm font-semibold leading-6 text-gray-900"
+              className="text-sm font-semibold leading-6 text-gray-900 text-xl"
             >
               {item.name}
             </a>
@@ -85,7 +85,7 @@ export default function Example() {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 text-xl"
                   >
                     {item.name}
                   </a>
