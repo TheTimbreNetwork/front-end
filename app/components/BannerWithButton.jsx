477d7976bf52ebb5b229db23518d9b88b6de8f55
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-09-07 17:36:28 -0400

    issue with banner centering css on sm sized screens

--- app/components/BannerWithButton.jsx
+++ app/components/BannerWithButton.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function BannerWithButton({ closeBanner }) {
   return (
-    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 before:flex-1">
       <div
         className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
         aria-hidden="true"
@@ -30,8 +30,7 @@ export function BannerWithButton({ closeBanner }) {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold lg:hidden">Get Help</strong>
-          <strong className="hidden font-semibold lg:inline">Get Help</strong>
+          <strong className="font-semibold">Get Help</strong>
           <svg
             viewBox="0 0 2 2"
             className="mx-2 inline hidden h-0.5 w-0.5 fill-current lg:inline"
