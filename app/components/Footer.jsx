2ef28b7a668f2d70c4f040c6ecded5f90438f5eb
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-09-08 19:32:04 -0400

    updated twitter url

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -76,7 +76,7 @@ export function Footer() {
           </form> */}
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com/timbre_network"
+              href="https://twitter.com/realTimbre"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
             />
